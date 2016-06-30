56bde28199268ef595e23a6fe70c465aee6d2626
Berlin Sky <pedroyoung8989@gmail.com>
2016-06-30 06:53:00 -0700

    add search capability.

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -9889,62 +9889,18 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 	// Search component:
 	var searchContainer = (0, _jquery2.default)('.js-searchContainer');
-	// const searchButton = $('.js-searchButton');
 
-	(0, _jquery2.default)('.js-searchButton').click(function () {
-		console.log('searchButton called');
-
+	(0, _jquery2.default)('.js-buttonSearchOpen').click(function () {
 		searchContainer.addClass('open');
 	});
 
-	var morphSearch = document.getElementById('morphsearch'),
-	    input = morphSearch.querySelector('input.morphsearch-input'),
-	    ctrlClose = morphSearch.querySelector('span.morphsearch-close'),
-	    isOpen = false,
-
-
-	// show/hide search area
-	toggleSearch = function toggleSearch(evt) {
-		// return if open and the input gets focused
-		if (evt.type.toLowerCase() === 'focus' && isOpen) return false;
-
-		if (isOpen) {
-			searchContainer.removeClass('open');
-
-			// trick to hide input text once the search overlay closes
-			// todo: hardcoded times, should be done after transition ends
-			if (input.value !== '') {
-				setTimeout(function () {
-					classie.add(morphSearch, 'hideInput');
-					setTimeout(function () {
-						classie.remove(morphSearch, 'hideInput');
-						input.value = '';
-					}, 300);
-				}, 500);
-			}
-
-			input.blur();
-		} else {
-			classie.add(morphSearch, 'open');
-		}
-		isOpen = !isOpen;
-	};
-
-	// events
-	input.addEventListener('focus', toggleSearch);
-	ctrlClose.addEventListener('click', toggleSearch);
-	// esc key closes search overlay
-	// keyboard navigation events
-	document.addEventListener('keydown', function (ev) {
-		var keyCode = ev.keyCode || ev.which;
-		if (keyCode === 27 && isOpen) {
-			toggleSearch(ev);
-		}
+	(0, _jquery2.default)('.js-buttonSearchClose').click(function () {
+		searchContainer.removeClass('open');
 	});
 
-	/***** for demo purposes only: don't allow to submit the form *****/
-	morphSearch.querySelector('button[type="submit"]').addEventListener('click', function (ev) {
-		ev.preventDefault();
+	(0, _jquery2.default)('.js-buttonSearchSubmit').click(function (evt) {
+		// TODO: add actual
+		evt.preventDefault();
 	});
 });
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,63 +16,24 @@ $(function () {
 	const ajax = new ajaxmini();
 	ajax.jsonPost();
 
-// Search component:
-const searchContainer = $('.js-searchContainer');
-// const searchButton = $('.js-searchButton');
+	// Search component:
+	const searchContainer = $('.js-searchContainer');
 
-$('.js-searchButton').click(function() {
-	console.log('searchButton called');
-	
-	searchContainer.addClass('open');
+	$('.js-buttonSearchOpen').click(function() {
+		searchContainer.addClass('open');
+	});
+
+	$('.js-buttonSearchClose').click(function() {
+		searchContainer.removeClass('open');
+	});
+
+	$('.js-buttonSearchSubmit').click(function(evt) {
+		// TODO: add actual
+		evt.preventDefault();
+	});
+
 });
 
 
-var morphSearch = document.getElementById( 'morphsearch' ),
-	input = morphSearch.querySelector( 'input.morphsearch-input' ),
-	ctrlClose = morphSearch.querySelector( 'span.morphsearch-close' ),
-	isOpen = false,
-
-	// show/hide search area
-	toggleSearch = function(evt) {
-		// return if open and the input gets focused
-		if( evt.type.toLowerCase() === 'focus' && isOpen ) return false;
-
-		if( isOpen ) {
-			searchContainer.removeClass( 'open' );
-
-			// trick to hide input text once the search overlay closes 
-			// todo: hardcoded times, should be done after transition ends
-			if( input.value !== '' ) {
-				setTimeout(function() {
-					classie.add( morphSearch, 'hideInput' );
-					setTimeout(function() {
-						classie.remove( morphSearch, 'hideInput' );
-						input.value = '';
-					}, 300 );
-				}, 500);
-			}
-			
-			input.blur();
-		}
-		else {
-			classie.add( morphSearch, 'open' );
-		}
-		isOpen = !isOpen;
-	};
-
-// events
-input.addEventListener( 'focus', toggleSearch );
-ctrlClose.addEventListener( 'click', toggleSearch );
-// esc key closes search overlay
-// keyboard navigation events
-document.addEventListener( 'keydown', function( ev ) {
-	var keyCode = ev.keyCode || ev.which;
-	if( keyCode === 27 && isOpen ) {
-		toggleSearch(ev);
-	}
-} );
 
 
-/***** for demo purposes only: don't allow to submit the form *****/
-morphSearch.querySelector( 'button[type="submit"]' ).addEventListener( 'click', function(ev) { ev.preventDefault(); } );
-});
