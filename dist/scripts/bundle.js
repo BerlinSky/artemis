814796ae6ca6870bdccbafb34e40e063bd7c1272
Berlin Sky <pedroyoung8989@gmail.com>
2016-06-29 23:12:31 -0700

    add ajaxmini

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -9886,6 +9886,59 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 	var ajax = new _ajaxmini2.default();
 	ajax.jsonPost();
+
+	// Search component:
+	var searchContainer = (0, _jquery2.default)('.js-searchContainer');
+
+	var morphSearch = document.getElementById('morphsearch'),
+	    input = morphSearch.querySelector('input.morphsearch-input'),
+	    ctrlClose = morphSearch.querySelector('span.morphsearch-close'),
+	    isOpen = false,
+
+
+	// show/hide search area
+	toggleSearch = function toggleSearch(evt) {
+		// return if open and the input gets focused
+		if (evt.type.toLowerCase() === 'focus' && isOpen) return false;
+
+		if (isOpen) {
+			searchContainer.removeClass('open');
+
+			// trick to hide input text once the search overlay closes
+			// todo: hardcoded times, should be done after transition ends
+			if (input.value !== '') {
+				setTimeout(function () {
+					classie.add(morphSearch, 'hideInput');
+					setTimeout(function () {
+						classie.remove(morphSearch, 'hideInput');
+						input.value = '';
+					}, 300);
+				}, 500);
+			}
+
+			input.blur();
+		} else {
+			classie.add(morphSearch, 'open');
+		}
+		isOpen = !isOpen;
+	};
+
+	// events
+	input.addEventListener('focus', toggleSearch);
+	ctrlClose.addEventListener('click', toggleSearch);
+	// esc key closes search overlay
+	// keyboard navigation events
+	document.addEventListener('keydown', function (ev) {
+		var keyCode = ev.keyCode || ev.which;
+		if (keyCode === 27 && isOpen) {
+			toggleSearch(ev);
+		}
+	});
+
+	/***** for demo purposes only: don't allow to submit the form *****/
+	morphSearch.querySelector('button[type="submit"]').addEventListener('click', function (ev) {
+		ev.preventDefault();
+	});
 });
 
 },{"./ajax/ajaxmini":2,"jquery":1}]},{},[3])
